69ca9224df85f724a0b7d9ab0789cfe7e411a99f
Henrique Caltram <henrivai@hotmail.com>
2022-03-08 16:36:01 -0300

    Build removeFromArray exercise file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+  for (let i = 1; i < arguments.length; i++) {
+    let element = arguments[i];
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === element) {
+        let subArrayBeforeElement = array.slice(0, j);
+        let subArrayAfterElement = array.slice(j + 1, array.length);
+        array = subArrayBeforeElement.concat(subArrayAfterElement);
+        break;
+      };
+    };
+  };
 
+  return array;
 };
 
 // Do not edit below this line
