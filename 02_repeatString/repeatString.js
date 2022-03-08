7a685fd4d1651d4c65d3739e6ae9728c1d443c1e
Henrique Caltram <henrivai@hotmail.com>
2022-03-08 15:17:46 -0300

    Build repeatString exercise file

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+  let expectedString = '';
 
+  if (negativeRepetitions(repetitions)) {
+    return 'ERROR'
+  };
+
+  for (let i = 0; i < repetitions; i++) {
+    expectedString += string;
+  };
+
+  return expectedString;
+};
+
+function negativeRepetitions(repetitions) {
+  return repetitions < 0;
 };
 
 // Do not edit below this line
