2f835c4ed35dfd960d23998ec4e06a25d215c0b4
Henrique Caltram <henrivai@hotmail.com>
2022-03-11 14:01:51 -0300

    Build leapYear function, encapsulate conditional business rule and unskip specs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  return (YearDivisable(year, 4) && !YearDivisable(year, 100) || YearDivisable(year, 400))
+};
 
+function YearDivisable(numerator, denominator) {
+  return (numerator % denominator == 0);
 };
 
 // Do not edit below this line
