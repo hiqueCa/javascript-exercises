2113317446803c5e07ebc8620e6326453b73be3c
Henrique Caltram <henrivai@hotmail.com>
2022-03-11 08:51:57 -0300

    Refactor functions as to encapsulate separate behaviors and make the main function more readable and expressive

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,27 +1,47 @@
 const sumAll = function(number1, number2) {
   let sum = 0;
-  auxNum = 0;
 
-  if ((number1 < 0) || (number2 < 0)) {
+  if (anyArgumentIsNotANumber(arguments) || anyNumberIsNegative(arguments)) {
     return 'ERROR';
-  };
+  }
 
-  if ((typeof(number1) != 'number') || (typeof(number2) != 'number')) {
-    return 'ERROR';
-  };
+  let crescOrderedNumbers = reorderNumberArgumentsCrescently(arguments)
 
-  if (number1 > number2) {
-    auxNum = number2;
-    number2 = number1;
-    number1 = auxNum;
-  };
-
-  for (let i = number1; i <= number2; i++) {
+  for (let i = crescOrderedNumbers[0]; i <= crescOrderedNumbers[arguments.length - 1]; i++) {
     sum += i;
   };
 
   return sum;
 };
+
+function anyNumberIsNegative(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0) {
+      return true;
+    };
+  };
+};
+
+function anyArgumentIsNotANumber(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (typeof(numbers[i]) != 'number') {
+      return true;
+    };
+  };
+};
+
+function reorderNumberArgumentsCrescently(numbers) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    let aux = 0;
+    if (numbers[i] > numbers[i+1]) {
+      aux = numbers[i+1]
+      numbers[i+1] = numbers[i]
+      numbers[i] = aux
+    };
+  };
+
+  return numbers
+}
 
 // Do not edit below this line
 module.exports = sumAll;
