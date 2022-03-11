7d8b8dc5b8074d44c474af5a4adf84b3761cd139
Henrique Caltram <henrivai@hotmail.com>
2022-03-11 14:29:52 -0300

    Build upon tempConversion exercise functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let roundedNumber = Math.round(((temp - 32) * 5/9) * 10) / 10;
+  return roundedNumber;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let roundedNumber = Math.round(((9/5 * temp) + 32) * 10) / 10;
+  return roundedNumber;
 };
 
 // Do not edit below this line
